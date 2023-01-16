9502e3d52be6230ae525496fb5ce887511335587
Numan Usman <92387051+Nuestman@users.noreply.github.com>
2023-01-16 10:14:28 +0000

    Add files via upload
    Fixed button colors not showing.

--- js/script.js
+++ js/script.js
@@ -12,14 +12,14 @@ function openMe(str, storyName) {
 
   
     // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+    // tablinks = document.getElementsByClassName("tablinks");
+    // for (i = 0; i < tablinks.length; i++) {
+    //   tablinks[i].className = tablinks[i].className.replace(" active", "");
+    // }
   
     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(storyName).style.display = "block";
-    str.currentTarget.className += " active";
+    // document.getElementById(storyName).style.display = "block";
+    // str.currentTarget.className += " active";
   }
 
 
